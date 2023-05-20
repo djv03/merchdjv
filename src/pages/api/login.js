5c9a221f9f49f39b453c8989bb3fc271ee6d8785
dhruvin <dhruvin.200410116086@gmail.com>
2023-05-20 23:50:05 +0530

    password encryption implemented

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,25 +1,27 @@
 import User from "../../../modals/User";
 import connectDB from "../../../middleware/mongoose";
+import CryptoJS from "crypto-js";
 
 async function handler(req, res) {
     if (req.method == 'POST') {
 
-        console.log(req.body);
-      
-        let user= await User.findOne({"email":req.body.email,})
-        if(user){
-            if (req.body.email== user.email && req.body.password== user.password) {
-                res.status(200).json({sucess:true,email:user.email,name:user.name})
+        // console.log(req.body);
+
+        let user = await User.findOne({ "email": req.body.email })
+        var bytes = CryptoJS.AES.decrypt(user.password, 'parita123');
+        // console.log(bytes);
+        var decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
+        // console.log(decryptedPass);
+        if (user) {
+            if (req.body.email == user.email && req.body.password == decryptedPass) {
+                res.status(200).json({ sucess: true, email: user.email, name: user.name })
             }
-
-            else{
-                res.status(200).json({sucess:false,error:"invalid crendentials"})
-
+            else {
+                res.status(200).json({ sucess: false, error: "invalid crendentials" })
             }
         }
-        else{
-            res.status(200).json({sucess:false,error:"no user found for that email"})
-
+        else {
+            res.status(200).json({ sucess: false, error: "no user found for that email" })
         }
     }
     else {
--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -1,13 +1,15 @@
 import User from "../../../modals/User";
 import connectDB from "../../../middleware/mongoose";
-
+var CryptoJS = require('crypto-js')
 async function handler(req, res) {
     if (req.method == 'POST') {
 
-        console.log(req.body);
-        res.status(200).json({ sucess: "added sucessfully in djvmerchF>products" })
-        let u= new User(req.body)
+        // console.log(req.body);
+        
+        const { name, email } = req.body
+        let u = new User({ name, email, password: CryptoJS.AES.encrypt(req.body.password, "parita123").toString() })
         await u.save()
+        res.status(200).json({ sucess: "sucessfully created new user" })
     }
     else {
         res.status(400).json({ error: "this method is not allowed" })
